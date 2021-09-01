7de8ccd1f7c66d578c17eb1f19a459391e5c30ef
Rene <zrpalano@globe.com.ph>
2021-09-01 22:38:49 +0800

    Done connecting to sql server

--- src/main/java/MavenTutorial/DataBaseConnection.java
+++ src/main/java/MavenTutorial/DataBaseConnection.java
@@ -1,7 +1,24 @@
 package MavenTutorial;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DataBaseConnection {
-	
-	
+
+	public static void main(String[] args) throws SQLException {
+
+		String host = "localhost";
+		String port = "3306";
+
+		Connection connection = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/Qadbt", "zkf00043", "Sun$trike1");
+		
+		Statement statement = connection.createStatement(); 
+		ResultSet resultSet = statement.executeQuery("select * from EmployeeInfo where name = 'Steve';");
+		resultSet.next();
+		System.out.println(resultSet.getString("location"));
+	}
 
 }
